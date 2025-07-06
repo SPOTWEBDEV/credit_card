ecf8be650accc84a43bb43fbc9161edc1ff05fdb
somto ogbodo <somtoogbodo2007@gmail.com>
2025-07-06 12:00:39 +0100

    pages

--- user/components/sidenav.php
+++ user/components/sidenav.php
@@ -37,50 +37,19 @@
                      </a>
                  </li>
 
-                 <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Pages</span></li>
 
                  <li class="nav-item">
                      <a class="nav-link menu-link" href="#sidebarInvoiceManagement" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarInvoiceManagement">
-                         <i class="las la-file-invoice"></i> <span data-key="t-invoices">Invoices Management</span>
+                         <i class="las la-file-invoice"></i> <span data-key="t-invoices">Deposit</span>
                      </a>
                      <div class="collapse menu-dropdown" id="sidebarInvoiceManagement">
                          <ul class="nav nav-sm flex-column">
                              <li class="nav-item">
-                                 <a href="invoice.html" class="nav-link" data-key="t-invoice"> Invoice </a>
+                                 <a href="invoice.html" class="nav-link" data-key="t-invoice"> Request</a>
                              </li>
 
                              <li class="nav-item">
-                                 <a href="invoice-add.html" class="nav-link" data-key="t-add-invoice"> Add Invoice </a>
-                             </li>
-
-
-                             <li class="nav-item">
-                                 <a href="invoice-details.html" class="nav-link" data-key="t-invoice-details"> Invoice Details </a>
-                             </li>
-
-
-
-                             <li class="nav-item">
-                                 <a class="nav-link menu-link" href="#sidebarReport" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarReport"
-                                     data-key="t-report">Report
-                                 </a>
-                                 <div class="collapse menu-dropdown" id="sidebarReport">
-                                     <ul class="nav nav-sm flex-column">
-                                         <li class="nav-item">
-                                             <a href="payment-summary.html" class="nav-link" data-key="t-payment-summary"> Payment Summary </a>
-                                         </li>
-                                         <li class="nav-item">
-                                             <a href="sale-report.html" class="nav-link" data-key="t-sale-report"> Sale Report </a>
-                                         </li>
-                                         <li class="nav-item">
-                                             <a href="expenses-report.html" class="nav-link" data-key="t-expenses-report"> Expenses Report </a>
-                                         </li>
-                                     </ul>
-                                 </div>
-                             </li>
-
-                             <li class="nav-item">
-                                 <a href="user.html" class="nav-link" data-key="t-users">Users</a>
+                                 <a href="invoice-add.html" class="nav-link" data-key="t-add-invoice"> History</a>
                              </li>
 
 
@@ -89,71 +58,33 @@
                      </div>
                  </li>
 
-                 <li class="nav-item">
-                     <a class="nav-link menu-link" href="#sidebarAuthentication" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarAuthentication">
-                         <i class="las la-cog"></i> <span data-key="t-authentication">Authentication</span>
-                     </a>
-                     <div class="collapse menu-dropdown" id="sidebarAuthentication">
-                         <ul class="nav nav-sm flex-column">
-                             <li class="nav-item">
-                                 <a href="auth-signin.html" class="nav-link" data-key="t-signin">Sign In</a>
-                             </li>
-
-                         </ul>
-                     </div>
-                 </li>
+               
 
 
 
 
 
                  <li class="nav-item">
-                     <a class="nav-link menu-link" href="#sidebarTables" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarTables">
-                         <i class="las la-table"></i> <span data-key="t-tables">Tables</span>
+                     <a class="nav-link menu-link" href="#sidebarTables" >
+                         <i class="las la-table"></i> <span data-key="t-tables">Purchase</span>
                      </a>
-                     <div class="collapse menu-dropdown" id="sidebarTables">
-                         <ul class="nav nav-sm flex-column">
-                             <li class="nav-item">
-                                 <a href="tables-basic.html" class="nav-link" data-key="t-basic-tables">Basic Tables</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="tables-gridjs.html" class="nav-link" data-key="t-grid-js">Grid Js</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="tables-listjs.html" class="nav-link" data-key="t-list-js">List Js</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="tables-datatables.html" class="nav-link" data-key="t-datatables">Datatables </a>
-                             </li>
-                         </ul>
-                     </div>
+         
                  </li>
 
 
 
                  <li class="nav-item">
-                     <a class="nav-link menu-link" href="#sidebarIcons" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarIcons">
-                         <i class="las la-gift"></i> <span data-key="t-icons">Icons</span>
+                     <a class="nav-link menu-link" href="#sidebarIcons" >
+                         <i class="las la-gift"></i> <span data-key="t-icons">My Card</span>
                      </a>
-                     <div class="collapse menu-dropdown" id="sidebarIcons">
-                         <ul class="nav nav-sm flex-column">
-                             <li class="nav-item">
-                                 <a href="icons-remix.html" class="nav-link" data-key="t-remix">Remix</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="icons-boxicons.html" class="nav-link" data-key="t-boxicons">Boxicons</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="icons-materialdesign.html" class="nav-link" data-key="t-material-design">Material Design</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="icons-bootstrap.html" class="nav-link" data-key="t-bootstrap">Bootstrap</a>
-                             </li>
-                             <li class="nav-item">
-                                 <a href="icons-lineawesome.html" class="nav-link" data-key="t-line-awesome">Line Awesome</a>
-                             </li>
-                         </ul>
-                     </div>
+                  
+                 </li>
+
+                   <li class="nav-item">
+                     <a class="nav-link menu-link" href="#sidebarAuthentication" >
+                         <i class="las la-cog"></i> <span data-key="t-authentication">Setting</span>
+                     </a>
+                  
                  </li>
 
 
--- user/deposit/index.php
+++ user/deposit/index.php
@@ -208,7 +208,7 @@ include("../../server/connection.php");
     </div>
 
     <!-- Theme Settings -->
-    <div class="offcanvas offcanvas-end border-0" tabindex="-1" id="theme-settings-offcanvas">
+    <!-- <div class="offcanvas offcanvas-end border-0" tabindex="-1" id="theme-settings-offcanvas">
         <div class="d-flex align-items-center bg-primary bg-gradient p-3 offcanvas-header">
             <h5 class="m-0 me-2 text-white">Theme Customizer</h5>
 
@@ -679,7 +679,7 @@ include("../../server/connection.php");
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <!-- JAVASCRIPT -->
     <script src="<?php echo $domain ?>assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
